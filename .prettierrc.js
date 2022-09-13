d35749bc68e3ccc6ff67a0c4da940c48bc293f56
Junho Yeo <i@junho.io>
2022-09-13 22:58:58 +0900

    [init] Setup Prettier and TypeScript

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+
+  // @ianvs/prettier-plugin-sort-imports
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '@/(assets|components|hooks|pages|recoil|styles|utils)/(.*)$',
+    '@/(.*)$',
+    '^[./](.*)$',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [require('@ianvs/prettier-plugin-sort-imports')],
+};
