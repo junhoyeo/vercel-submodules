d872843c0536819bde5968880c17e2f624ce6e2a
Junho Yeo <i@junho.io>
2022-09-14 07:12:34 +0900

    [cli] Clone submodules one-by-one

--- src/commands/clone.ts
+++ src/commands/clone.ts
@@ -24,29 +24,27 @@ type CloneOptions = {
 export const clone = async ({ githubToken, depth, submodules }: CloneOptions) => {
   const rootDir = (await zx.$`pwd`).stdout.trim();
 
-  await Promise.all(
-    submodules.map(async (submodule) => {
-      const submoduleDir = path.join(rootDir, submodule.path);
+  for (const submodule of submodules) {
+    const submoduleDir = path.join(rootDir, submodule.path);
 
-      await zx.$`rm -rf ${submoduleDir}`.catch(() => {
-        /* ignored */
-      });
-      await zx.$`mkdir ${submoduleDir}`.catch(() => {
-        /* ignored */
-      });
+    await zx.$`rm -rf ${submoduleDir}`.catch(() => {
+      /* ignored */
+    });
+    await zx.$`mkdir ${submoduleDir}`.catch(() => {
+      /* ignored */
+    });
 
-      await insideDir(submoduleDir, async () => {
-        await zx.$`git init`;
-        try {
-          await zx.$`git remote add origin ${submodule.url}`;
-        } catch {
-          await zx.$`git remote set-url origin ${submodule.url}`;
-        }
-        await zx.$`git fetch origin ${submodule.commitHash} --depth=${depth} --no-tags`;
-        await zx.$`git reset --hard FETCH_HEAD`;
-      });
+    await insideDir(submoduleDir, async () => {
+      await zx.$`git init`;
+      try {
+        await zx.$`git remote add origin ${submodule.url}`;
+      } catch {
+        await zx.$`git remote set-url origin ${submodule.url}`;
+      }
+      await zx.$`git fetch origin ${submodule.commitHash} --depth=${depth} --no-tags`;
+      await zx.$`git reset --hard FETCH_HEAD`;
+    });
 
-      console.log(`${chalk.green('Clone Success')} Submodule ${chalk.cyan(`${submodule.path}@${submodule.commitHash.slice(0, 7)}`)}`);
-    }),
-  );
+    console.log(`${chalk.green('Clone Success')} Submodule ${chalk.cyan(`${submodule.path}@${submodule.commitHash.slice(0, 7)}`)}`);
+  }
 };
